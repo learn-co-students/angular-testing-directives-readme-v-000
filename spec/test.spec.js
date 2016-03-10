36d2144aa3dece0cc597cf513e14e96b9ce917a5
mitul <MitulMistryDev@gmail.com>
2016-03-10 11:36:35 -0600

    Done.

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,12 +1,13 @@
 describe('angularjs homepage todo list', function() {
-	it('should add a todo', function() {
-		browser.get('https://angularjs.org');
+	//test
+  it('should add a todo', function() {
+      browser.get('https://angularjs.org');
 
-		element(by.model('todoList.todoText')).sendKeys('Writing tests!!');
-		element(by.css('[value="add"]')).click();
+      element(by.model('todoList.todoText')).sendKeys('Writing tests!!');
+      element(by.css('[value="add"]')).click();
 
-		var todoList = element.all(by.repeater('todo in todoList.todos'));
-		expect(todoList.count()).toEqual(3);
-		expect(todoList.get(2).getText()).toEqual('Writing tests!!');
-	});
+      var todoList = element.all(by.repeater('todo in todoList.todos'));
+      expect(todoList.count()).toEqual(3);
+      expect(todoList.get(2).getText()).toEqual('Writing tests!!');
+  });
 });
