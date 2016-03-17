a5d91a9e841dfeb0f9a8e5f7f455a1ae7ca1e5d4
Chen Yang <yangc5@mail.uc.edu>
2016-03-17 14:35:28 +0000

    done

--- conf.js
+++ conf.js
@@ -1,4 +1,9 @@
 exports.config = {
-	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs: ['spec/**/*.spec.js']
+	seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+	specs: ['spec/**/*.spec.js'],
+  capabilities: {
+    'browserName': 'phantomjs',
+    'phantomjs.binary.path': require('phantomjs-prebuilt').path,
+    'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
+  }
 };
