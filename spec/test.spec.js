3bfee820b930e3ad6d7b7572b9dd65b71c4463a6
jasoncasares <jasonc@csdrafting.com>
2016-10-18 12:34:21 -0700

    Done.

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,12 +1,12 @@
 describe('angularjs homepage todo list', function() {
-	it('should add a todo', function() {
-		browser.get('https://angularjs.org');
+    it('should add a todo', function() {
+        browser.get('https://angularjs.org');
 
-		element(by.model('todoList.todoText')).sendKeys('Writing tests!!');
-		element(by.css('[value="add"]')).click();
+        element(by.model('todoList.todoText')).sendKeys('Writing tests!!');
+        element(by.css('[value="add"]')).click();
 
-		var todoList = element.all(by.repeater('todo in todoList.todos'));
-		expect(todoList.count()).toEqual(3);
-		expect(todoList.get(2).getText()).toEqual('Writing tests!!');
-	});
+        var todoList = element.all(by.repeater('todo in todoList.todos'));
+        expect(todoList.count()).toEqual(3);
+        expect(todoList.get(2).getText()).toEqual('Writing tests!!');
+    });
 });
