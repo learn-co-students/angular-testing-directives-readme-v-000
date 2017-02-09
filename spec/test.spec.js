18f14fcf43eda0820f06e7814027d19a137af2d5
MIKE-RANDELL <michael.james.randell@gmail.com>
2017-02-09 09:50:50 -0500

    Done.

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,4 +1,5 @@
 describe('angularjs homepage todo list', function() {
+
 	it('should add a todo', function() {
 		browser.get('https://angularjs.org');
 
@@ -8,5 +9,6 @@ describe('angularjs homepage todo list', function() {
 		var todoList = element.all(by.repeater('todo in todoList.todos'));
 		expect(todoList.count()).toEqual(3);
 		expect(todoList.get(2).getText()).toEqual('Writing tests!!');
+
 	});
 });
